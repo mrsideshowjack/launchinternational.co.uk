1014f4d29178d44b63410673b7668fd42a63f681
Jack Mason <mrsideshowjack@gmail.com>
2016-12-01 13:32:39 +0000

    meeting final

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp');
 var webserver = require('gulp-webserver');
+var imagemin = require('gulp-imagemin');
+var htmlmin = require('gulp-htmlmin');
+var cleanCSS = require('gulp-clean-css');
+var runSequence = require('run-sequence');
  
 gulp.task('serve', function() {
   gulp.src('./')
@@ -10,4 +14,33 @@ gulp.task('serve', function() {
       directoryListing: true,
       open: true
     }));
+});
+
+gulp.task('build', function(callback) {
+  runSequence('imgmin',
+              'htmlmin',
+              'cssmin',
+              callback);
+});
+
+gulp.task('imgmin', () =>
+    gulp.src('images/*/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'))
+);
+
+gulp.task('htmlmin', function() {
+  return gulp.src('*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('cssmin', function() {
+  return gulp.src('styles/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(cleanCSS({debug: true}, function(details) {
+            console.log(details.name + ': ' + details.stats.originalSize);
+            console.log(details.name + ': ' + details.stats.minifiedSize);
+        }))
+    .pipe(gulp.dest('dist/styles'));
 });
